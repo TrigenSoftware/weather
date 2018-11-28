d39e6b3f3d12e2d42ac3fcdc2ef6044373110774
dangreen <danon0404@gmail.com>
2018-11-28 11:32:05 +0700

    test dir -> spec files, one more spec file

---
+++ src/App/services/weather/spec.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { getCurrentWeather } from './';
+
+describe('API', () => {
+
+	describe('Weather Service', () => {
+
+		describe('getCurrentWeather', () => {
+
+			it('should get correct weather data', async () => {
+
+				const currentWeather = await getCurrentWeather('Novosibirsk');
+
+				expect(typeof currentWeather.date).toBe('string');
+				expect(typeof currentWeather.temp).toBe('number');
+				expect(typeof currentWeather.description).toBe('string');
+				expect(typeof currentWeather.humidity).toBe('number');
+				expect(typeof currentWeather.clouds).toBe('number');
+				expect(typeof currentWeather.precipitation).toBe('number');
+			});
+		});
+	});
+});
