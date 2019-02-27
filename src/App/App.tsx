3ea808f1200443dec0f587b417783835ace50ba2
Den Lebedinsky <Den.Lebedinsky@gmail.com>
2019-02-27 16:52:35 +0300

    fix tab

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,12 @@
 import React, {
- ReactChild,
- PureComponent
+	ReactChild,
+	PureComponent
 } from 'react';
 import { hot } from 'react-hot-loader';
 import {
- BrowserRouter as Router,
- Route,
- Link
+	BrowserRouter as Router,
+	Route,
+	Link
 } from 'react-router-dom';
 import Weather from '~/containers/Weather/loadable';
 import Todo from '~/containers/Todo/loadable';
@@ -22,60 +22,60 @@ export default class App extends PureComponent<IProps> {
 
 	render() {
 		return this.router(
-   <div
-    {...stylesheet('root')}
-   >
-    <Navigator>
-     <Link to='/'>
-      Home
-				</Link>
-     <Link to='/weather'>
-      Weather
-				</Link>
-     <Link to='/todo'>
-      Todo
-				</Link>
-    </Navigator>
-    <hr />
-    <Route
-     path='/'
-     exact
-     component={Home}
-    />
-    <Route
-     path='/weather'
-     exact
-     component={Weather}
-    />
-    <Route
-     path='/todo'
-     exact
-     component={Todo}
-    />
-   </div>
-  );
+			<div
+				{...stylesheet('root')}
+			>
+				<Navigator>
+					<Link to='/'>
+						Home
+					</Link>
+					<Link to='/weather'>
+						Weather
+					</Link>
+					<Link to='/todo'>
+						Todo
+					</Link>
+				</Navigator>
+				<hr />
+				<Route
+					path='/'
+					exact
+					component={Home}
+				/>
+				<Route
+					path='/weather'
+					exact
+					component={Weather}
+				/>
+				<Route
+					path='/todo'
+					exact
+					component={Todo}
+				/>
+			</div>
+		);
 	}
 
 	router(children: ReactChild) {
 
 		const {
-   disableRouter = false
-  } = this.props;
+			disableRouter = false
+		} = this.props;
 
 		if (disableRouter) {
 			return children;
 		}
 
 		return (
-   <Router>
-    {children}
-   </Router>
+			<Router>
+				{children}
+			</Router>
 		);
 	}
 }
 
 function Home() {
 	return (
-  <h2>Home</h2>
+		<h2>Home</h2>
 	);
 }
