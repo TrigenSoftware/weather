26c164cc7097944f3777775f1d25500c03c88a10
dangreen <danon0404@gmail.com>
2019-03-07 13:19:22 +0700

    wip: problems with sw loader and hmr

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from '@flexis/redux';
+// @ts-ignore
+import registerServiceWorker from './sw';
 import App from './App';
 import createStore from './store';
 
 async function main() {
+
+	registerServiceWorker({ scope: '/' }).then(() => {
+
+		console.log('Success!');
+
+		navigator.serviceWorker.addEventListener('message', (event) => {
+			console.log(JSON.stringify(event.data, null, '\t'));
+		});
+	});
 
 	const root = document.querySelector('#view');
 	const store = createStore();
