88ed66ec3a8279c4bf62e441ad2a34515f4c59bd
dangreen <danon0404@gmail.com>
2019-03-19 02:40:39 +0700

    mocks for services test

--- src/App/services/weather/spec.ts
+++ src/App/services/weather/spec.ts
@@ -1,13 +1,56 @@
 import 'dotenv/config';
+import moxios from 'moxios';
+import weatherClient from './client';
 import { getCurrentWeather } from './';
 
+const MOCK_WEATHER_RESPONSE = {
+	weather: [{
+		description: 'clear sky'
+	}],
+	main:    {
+		temp:     -6,
+		humidity: 79
+	},
+	clouds: {
+		all: 0
+	},
+	snow:   {
+		'3h': 0.01
+	},
+	dt_txt: '2019-03-18 21:00:00'
+};
+const MOCK_FORECAST_RESPONSE = {
+	list: [
+		MOCK_WEATHER_RESPONSE,
+		MOCK_WEATHER_RESPONSE
+	]
+};
+
 describe('API', () => {
+
+	beforeEach(() => {
+		moxios.install(weatherClient);
+	});
+
+	afterEach(() => {
+		moxios.uninstall(weatherClient);
+	});
 
 	describe('Weather Service', () => {
 
 		describe('getCurrentWeather', () => {
 
 			it('should get correct weather data', async () => {
+
+				moxios.stubRequest(/\/weather/, {
+					status:    200,
+					response: MOCK_WEATHER_RESPONSE
+				});
+
+				moxios.stubRequest(/\/forecast/, {
+					status:    200,
+					response: MOCK_FORECAST_RESPONSE
+				});
 
 				const currentWeather = await getCurrentWeather('Novosibirsk');
 
