b6050959a3a0a13a693d86bcfe10bd80007dc6d6
dangreen <danon0404@gmail.com>
2019-05-28 14:12:22 +0700

    rollback deps

--- src/App/components/WeatherList/WeatherList.stories.tsx
+++ src/App/components/WeatherList/WeatherList.stories.tsx
@@ -1,55 +1,55 @@
-// /* tslint:disable:no-magic-numbers */
-// import React from 'react';
-// import {
-// 	storiesOf
-// } from '@storybook/react';
-// import {
-// 	number,
-// 	button
-// } from '@storybook/addon-knobs/react';
-// import Weather from '../Weather';
-// import {
-// 	getFakeData
-// } from '../Weather/Weather.mock';
-// import WeatherList from './';
+/* tslint:disable:no-magic-numbers */
+import React from 'react';
+import {
+	storiesOf
+} from '@storybook/react';
+import {
+	number,
+	button
+} from '@storybook/addon-knobs/react';
+import Weather from '../Weather';
+import {
+	getFakeData
+} from '../Weather/Weather.mock';
+import WeatherList from './';
 
-// const stylableApi = `
-// Stylable API
-// ---
-// - ::item
-// `;
+const stylableApi = `
+Stylable API
+---
+- ::item
+`;
 
-// storiesOf('WeatherList', module)
-// 	.addParameters({
-// 		info: stylableApi
-// 	})
-// 	.add(
-// 		'with items',
-// 		() => {
+storiesOf('WeatherList', module)
+	.addParameters({
+		info: stylableApi
+	})
+	.add(
+		'with items',
+		() => {
 
-// 			button('Generate new data', () => {});
+			button('Generate new data', () => {});
 
-// 			return (
-// 				<WeatherList>
-// 					{Array.from({
-// 						length: number('Items count', 3)
-// 					}).map((_, i) => {
+			return (
+				<WeatherList>
+					{Array.from({
+						length: number('Items count', 3)
+					}).map((_, i) => {
 
-// 						const fakeData = getFakeData();
+						const fakeData = getFakeData();
 
-// 						return (
-// 							<Weather
-// 								key={i}
-// 								date={fakeData.date}
-// 								temp={fakeData.temp}
-// 								description={fakeData.description}
-// 								humidity={fakeData.humidity}
-// 								clouds={fakeData.clouds}
-// 								precipitation={fakeData.precipitation}
-// 							/>
-// 						);
-// 					})}
-// 				</WeatherList>
-// 			);
-// 		}
-// 	);
+						return (
+							<Weather
+								key={i}
+								date={fakeData.date}
+								temp={fakeData.temp}
+								description={fakeData.description}
+								humidity={fakeData.humidity}
+								clouds={fakeData.clouds}
+								precipitation={fakeData.precipitation}
+							/>
+						);
+					})}
+				</WeatherList>
+			);
+		}
+	);
--- src/App/containers/Todo/loadable.ts
+++
@@ -1,7 +0,0 @@
-import loadable from 'react-loadable';
-import Loading from '~/components/Loading';
-
-export default loadable({
-	loader:  () => import('./connected'),
-	loading: Loading
-});
