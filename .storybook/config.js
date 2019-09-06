b27822bbfbfa8dd65411abc35dc9f328104c4818
dangreen <danon0404@gmail.com>
2019-09-06 17:01:21 +0300

    fix

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
+import faker from 'faker';
 import {
 	configure
 } from '@trigen/scripts-preset-react-app/storybook/config';
 import '@flexis/ui/reboot.st.css';
+
+if (process.env.SEED) {
+	faker.seed(parseInt(process.env.SEED));
+}
 
 const stories = require.context(
 	process.env.PROJECT_SRC,
