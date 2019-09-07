2fea13905d69ec64adb0bfc64341687a0b4ac6ea
dangreen <danon0404@gmail.com>
2019-09-07 06:45:53 +0300

    generate screenshots

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -1,17 +1,3 @@
-import initStoryshots, {
-	getMatchOptions
-} from '@trigen/scripts-plugin-storybook/jest/storyshots';
+import initStoryshots from '@trigen/scripts-plugin-storybook/jest/storyshots';
 
-initStoryshots({
-	getMatchOptions(info) {
-
-		const options = getMatchOptions(info);
-		const failureThreshold = 0.03;
-
-		return {
-			failureThresholdType: 'percent',
-			failureThreshold,
-			...options
-		};
-	}
-});
+initStoryshots();
