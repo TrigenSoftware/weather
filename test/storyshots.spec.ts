c5ac460de8cdb678ffea31dedf8bcdb324ca867e
dangreen <danon0404@gmail.com>
2019-09-12 16:16:23 +0700

    storyshots mod

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -1,6 +1,11 @@
 import initStoryshots from '@trigen/scripts-plugin-storybook/jest/storyshots';
 
 if (process.platform === 'linux') {
+
+	process.env.SEED = JSON.stringify(584);
+	process.env.DISABLE_BROWSER_SYNC = JSON.stringify(true);
+	jest.setTimeout(30000);
+
 	initStoryshots();
 } else {
 	it('should skip storyshots', () => {});
